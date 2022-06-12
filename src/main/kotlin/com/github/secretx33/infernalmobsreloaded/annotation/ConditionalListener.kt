068662fcd244e113d52f46db99f599941248571c
SecretX33 <4389757+SecretX33@users.noreply.github.com>
2022-06-12 13:04:08 -0300

    Added ConditionalListener annotation

---
+++ src/main/kotlin/com/github/secretx33/infernalmobsreloaded/annotation/ConditionalListener.kt
@@ -0,0 +1,11 @@
+package com.github.secretx33.infernalmobsreloaded.annotation
+
+import org.bukkit.event.Listener
+
+/**
+ * If a [Listener] class is annotated with `ConditionalListener`, it won't be automatically discovered in the
+ * classpath scan, thus requiring manual registering later.
+ */
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ConditionalListener
